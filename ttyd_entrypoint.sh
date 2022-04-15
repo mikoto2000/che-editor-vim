eb1934fa43f59d04af1be2d9f916cc8ffa7269df
mikoto2000 <mikoto2000@gmail.com>
2022-04-16 05:21:08 +0900

    vim の設定ファイルを読み込む仕組みを追加。

--- ttyd_entrypoint.sh
+++ ttyd_entrypoint.sh
@@ -4,7 +4,18 @@ echo "Start project clone..."
 che-project-cloner
 echo "done."
 
+echo "Create link if _vim exists..."
+if [ -d ~/_vim ]; then
+    if [ ! -L ~/.vim ]; then
+        ln -s ~/_vim ~/.vim
+    fi
+fi
+echo "done."
+
+
 watch -n 180 curl -XPUT ${CHE_API_EXTERNAL}/activity/${CHE_WORKSPACE_ID}?token=${CHE_MACHINE_TOKEN} > /dev/null &
+
+clear
 
 while true
 do
