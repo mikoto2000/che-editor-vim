d270234dfc50f41e1f1610137939c9d50d878feb
mikoto2000 <mikoto2000@gmail.com>
2022-04-21 18:10:32 +0900

    自己証明書の Che インスタンスへの暫定対応のために証明書検証を無効化。

--- ttyd_entrypoint.sh
+++ ttyd_entrypoint.sh
@@ -13,7 +13,7 @@ fi
 echo "done."
 
 
-watch -n 180 curl -XPUT ${CHE_API_EXTERNAL}/activity/${CHE_WORKSPACE_ID}?token=${CHE_MACHINE_TOKEN} > /dev/null &
+watch -n 180 curl --insecure -XPUT ${CHE_API_EXTERNAL}/activity/${CHE_WORKSPACE_ID}?token=${CHE_MACHINE_TOKEN} > /dev/null &
 
 clear
 
